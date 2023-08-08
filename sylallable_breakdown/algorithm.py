4c7972126d4e1461946d0b0547e54787f1ee55a3
Parama Wattanakrai <Parama.Wattanakrai@gmail.com>
2023-08-08 09:30:26 +0700

    Algorithm Classes, unfinished

--- sylallable_breakdown/algorithm.py
+++ sylallable_breakdown/algorithm.py
@@ -1,29 +1,39 @@
 from dict import *
+LEADING_CHARACTERS = dict[consonant_classes]
 
-text = ('เคราะห์')
-no_tones = []
-text_lists = []
-text_vowels = []
-split_indexes = []
-syllables = []
-
-class ThaiCharacter:
-    def __init__(self, character, position, before, after):
-        self.character = character
+class Character:
+    def __init__(self, char, position, cluster_role, char_role, before, after):
+        self.character = char
         self.position = position
+        self.role = role
         self.before = before
         self.after = after
-    def information(self):
-        information = f"Character: {self.position}\nPosition: {self.character}\nChracters Before: {self.before}\nCharacters After: {self.after}"
-        return information
+    def getInformation(self):
+        return f'Character: {self.char}\nPosition: {self.position}\nRole: {self.role}\nChracters Before: {self.before}\nCharacters After: {self.after}'
     def getBefore(self, distance):
-        if len(self.before):
-            return "No character there"
+        distance += 1
+        if len(self.before) < distance:
+            return None
         return self.before[-distance]
     def getAfter(self, distance):
         if len(self.after) < distance:
-            return "No character there"
+            return None
         return self.after[distance]
 
-char = ThaiCharacter('ก', 1, ['เ'], ['ร','ี','ย','น'])
-print(char.getAfter(2))
+class Syllable:
+    def __init__(self, string):
+        self.chars = []
+        for index, char in enumerate(string):
+            before = string[:index]
+            after = string[index+1:]
+            self.chars.append(Character(char, index, 'unknown', 'unknown', before, after))
+    def getInformation(self):
+        information = f''
+        return information
+
+class Cluster:
+    def __init__(self, cluster_role, chars):
+        self.cluster_role = cluster_role
+        self.chars = chars
+    def findCharRole(self, target_char_role):
+        return next((char for char in self.chars if char.char_role == target_char_role), None)
--- sylallable_breakdown/dict.py
+++ sylallable_breakdown/dict.py
@@ -34,3 +34,28 @@ th_utils = {
         'ว': {'ก','ข','ค'}
     }
 }
+
+th_all = {
+    'consonant_classes': {
+        'low_classes': {
+            'low_paireds': {'ค','ฅ','ฆ','ช','ซ','ฌ','ฑ','ฒ','ท','ธ','พ','ฟ','ภ','ฮ'},
+            'low_unpaireds': {'ง','ญ','ณ','น','ม','ย','ร','ฤ','ล','ฦ','ว','ฬ'}
+            },
+        'mid_classes': {'ก','จ','ฎ','ฏ','ด','ต','บ','ป','อ'},
+        'high_classes': {'ข','ฃ','ฉ','ฐ','ถ','ผ','ฝ','ศ','ษ','ส','ห'},
+    },
+    'leading_consonants': {'ห','อ'}
+}
+
+class ThaiCharDict:
+    def __init__(self, consonants, vowels, tone_marks,
+                low_consonants, unpaired_low_consonants, paired_low_consonants, high_consonants, mid_consonants,
+                leading_consonants,
+                leading_consonants, following_consonants):
+        self.low_consonants = low_consonants
+        self.unpaired_low_consonants = unpaired_low_consonants
+        self.paired_low_consonants = paired_low_consonants
+        self.high_consonants = high_consonants
+        self.mid_consonants = mid_consonants
+
+        self.leading_consonants = leading_consonants
