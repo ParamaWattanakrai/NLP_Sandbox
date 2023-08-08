57c249d444554664e5167382729db9e8b98a6178
Parama Wattanakrai <Parama.Wattanakrai@gmail.com>
2023-08-08 11:56:14 +0700

    pointed list, broken

--- sylallable_breakdown/dict.py
+++ sylallable_breakdown/dict.py
@@ -35,23 +35,32 @@ th_utils = {
     }
 }
 
-th_all = {
-    'consonant_classes': {
-        'low_classes': {
-            'low_paireds': {'ค','ฅ','ฆ','ช','ซ','ฌ','ฑ','ฒ','ท','ธ','พ','ฟ','ภ','ฮ'},
-            'low_unpaireds': {'ง','ญ','ณ','น','ม','ย','ร','ฤ','ล','ฦ','ว','ฬ'}
-            },
-        'mid_classes': {'ก','จ','ฎ','ฏ','ด','ต','บ','ป','อ'},
-        'high_classes': {'ข','ฃ','ฉ','ฐ','ถ','ผ','ฝ','ศ','ษ','ส','ห'},
-    },
-    'leading_consonants': {'ห','อ'}
-}
+consonants = ['ก', 'ข', 'ฃ', 'ค', 'ฅ', 'ฆ', 'ง', 'จ', 'ฉ', 'ช',
+    'ซ', 'ฌ', 'ญ', 'ฎ', 'ฏ', 'ฐ', 'ฑ', 'ฒ', 'ณ', 'ด',
+    'ต', 'ถ', 'ท', 'ธ', 'น', 'บ', 'ป', 'ผ', 'ฝ', 'พ',
+    'ฟ', 'ภ', 'ม', 'ย', 'ร', 'ฤ', 'ล', 'ฦ', 'ว', 'ศ',
+    'ษ', 'ส', 'ห', 'ฬ', 'อ', 'ฮ']
+vowels = ['ิ', 'ี', 'ึ', 'ื', 'ๅ', 'ุ', 'ู', 'เ', 'โ', 'แ',
+    'ะ', 'ั', 'า', 'ำ', 'ใ', 'ไ', '็']
+tone_marks = ['่','้','๊','๋']
+
+
+low_consonants = []
+unpaired_low_consonants = []
+paired_low_consonants = []
+high_consonants = []
+mid_consonants = []
+
+leading_consonants = ['ห','อ']
+
+blends = []
+blend_initials = []
 
 class ThaiCharDict:
     def __init__(self, consonants, vowels, tone_marks,
-                low_consonants, unpaired_low_consonants, paired_low_consonants, high_consonants, mid_consonants,
-                leading_consonants,
-                leading_consonants, following_consonants):
+            low_consonants, unpaired_low_consonants, paired_low_consonants, high_consonants, mid_consonants,
+            leading_consonants,
+            blends, blend_initials):
         self.low_consonants = low_consonants
         self.unpaired_low_consonants = unpaired_low_consonants
         self.paired_low_consonants = paired_low_consonants
@@ -59,3 +68,8 @@ class ThaiCharDict:
         self.mid_consonants = mid_consonants
 
         self.leading_consonants = leading_consonants
+
+th_all = ThaiCharDict(consonants, vowels, tone_marks,
+            low_consonants, unpaired_low_consonants, paired_low_consonants, high_consonants, mid_consonants,
+            leading_consonants,
+            blends, blend_initials)
