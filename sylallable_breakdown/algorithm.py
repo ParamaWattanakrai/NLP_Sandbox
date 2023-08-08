9fece644dfc2172aea0a48f0b5e9d7dd1ad270c5
Parama Wattanakrai <Parama.Wattanakrai@gmail.com>
2023-08-08 11:56:45 +0700

    class

--- sylallable_breakdown/algorithm.py
+++ sylallable_breakdown/algorithm.py
@@ -1,15 +1,24 @@
 from dict import *
-LEADING_CHARACTERS = dict[consonant_classes]
+LEADING_CHARACTERS = dict
 
 class Character:
     def __init__(self, char, position, cluster_role, char_role, before, after):
-        self.character = char
+        self.char = char
         self.position = position
-        self.role = role
+        self.cluster_role = cluster_role
+        self.char_role = char_role
         self.before = before
         self.after = after
     def getInformation(self):
-        return f'Character: {self.char}\nPosition: {self.position}\nRole: {self.role}\nChracters Before: {self.before}\nCharacters After: {self.after}'
+        chars_before = []
+        chars_after = []
+        for char in self.before:
+            chars_before.append(char.char)
+        for char in self.after:
+            chars_after.append(char.char)
+        print('hello')
+        print(self.char)
+        return f'Character: {self.char}\nPosition: {self.position}\nIn Cluster: {self.cluster_role}\nRole: {self.char_role}\nChracters Before: {chars_before}\nCharacters After: {chars_before}'
     def getBefore(self, distance):
         distance += 1
         if len(self.before) < distance:
@@ -24,9 +33,16 @@ class Syllable:
     def __init__(self, string):
         self.chars = []
         for index, char in enumerate(string):
-            before = string[:index]
-            after = string[index+1:]
-            self.chars.append(Character(char, index, 'unknown', 'unknown', before, after))
+            thischar = Character(char, index, 'unknown', 'unknown', self.chars, [])
+            if self.chars is None:
+                self.chars = [thischar]
+                continue
+            for char in self.chars:
+                if char.after is None:
+                    char.after = []
+                    continue
+                char.after = char.after.append(thischar)
+            self.chars.append(thischar)
     def getInformation(self):
         information = f''
         return information
@@ -37,3 +53,10 @@ class Cluster:
         self.chars = chars
     def findCharRole(self, target_char_role):
         return next((char for char in self.chars if char.char_role == target_char_role), None)
+
+def find_leading_consonant(syllable):
+    for char in syllable:
+        break
+
+syl = Syllable('เรียน')
+print(syl.chars[0].getInformation())
