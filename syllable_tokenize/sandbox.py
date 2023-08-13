b18710e45a4ec9b69fa22a5dcdac7d6e99d16de1
PNPengy <Parama.Wattanakrai@gmail.com>
2023-08-13 21:52:05 +0700

    custom dictionary

--- syllable_tokenize/sandbox.py
+++ syllable_tokenize/sandbox.py
@@ -1,3 +1,18 @@
-import pythainlp
+from pythainlp.tokenize import Tokenizer
 
-print(pythainlp.wangchanberta.segment('สวัสดี'))
+PATH_TO_CUSTOM_DICTIONARY = './custom_dictionary.txtt'
+
+# write a file
+# with open(PATH_TO_CUSTOM_DICTIONARY, 'w', encoding='utf-8') as f:
+#     f.write('อะเฟเซีย\nAphasia\nผิด\nปกติ')
+
+text = "อะเฟเซีย (Aphasia) เป็นอาการผิดปกติของการพูด"
+
+# initate an object from file with `attacut` as tokenizer
+_tokenizer = Tokenizer(custom_dict=PATH_TO_CUSTOM_DICTIONARY, \
+    engine='newmm')
+
+print(_tokenizer.word_tokenize('กรณี'))
+# output:
+# ['อะเฟเซีย', ' ', '(', 'Aphasia', ')', ' ', 'เป็น', 'อาการ', 'ผิด',
+#   'ปกติ', 'ของ', 'การ', 'พูด']
