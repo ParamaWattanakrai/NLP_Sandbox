8c9d61fcf962b399fd034eff67115b7e6b25b919
PNPengy <Parama.Wattanakrai@gmail.com>
2023-08-16 18:42:19 +0700

    reformat for segmentation

--- embedding.py
+++ embedding.py
@@ -1,14 +1,11 @@
 import os
 import csv
 import torch
-import random
 
 character_embeddings = {}
-dataset_dict = {}
 
 C_PATH = os.path.dirname(__file__)
-embedding_filepath = os.path.join(C_PATH, 'data/data_vectors100000x68re.csv')
-dataset_filepath = os.path.join(C_PATH, 'data/dataset.csv')
+embedding_filepath = os.path.join(C_PATH, 'feature/onehot68.csv')
 
 with open(embedding_filepath, 'r', encoding='utf-8') as f:
     unstructured_character_embeddings = csv.reader(f)
@@ -16,13 +13,7 @@ with open(embedding_filepath, 'r', encoding='utf-8') as f:
         if idx >= 1:
             character_embeddings[row[0]] = row[1:]
 
-with open(dataset_filepath, 'r', encoding='utf-8') as f:
-    unstructured_dataset = csv.reader(f)
-    for idx, row in enumerate(unstructured_dataset):
-        if idx >= 1:
-            dataset_dict[row[0]] = row[1:]
-
-def embedding(line):
+def embed(line):
     embedding = torch.zeros(len(line), 1, 68)
     for i, character in enumerate(line):
         character_vector = character_embeddings.get(character)
