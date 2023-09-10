e62fd6adbbb2f9d436e1a247cc2183d9234894ca
Thanakorn Boonyasena <thanakorn.boonyasena@gmail.com>
2023-09-10 19:38:33 +0700

    need help

--- New_CRF/crf.py
+++ New_CRF/crf.py
@@ -6,21 +6,13 @@ from torchcrf import CRF
 from utils import DataLoader, embed
 
 embedded = DataLoader()
-char_to_idx = {'<PAD>': 0,'<UNK>': 1}
-tag_to_idx = {'<PAD>': 0}
+tag_to_idx = {}
 for sent_tags in Y_train:
     for tag in sent_tags:
         if tag not in tag_to_idx:
             tag_to_idx[tag] = len(tag_to_idx)
 
-X_train_idx = [[char_to_idx.get(char, char_to_idx['<UNK>']) for char in word] for word in X_train]
 Y_train_idx = [[tag_to_idx[tag] for tag in word_tags] for word_tags in Y_train]
-
-# print(char_to_idx)
-# print(tag_to_idx)
-print(X_train)
-# print(X_train_idx)
-# print(Y_train_idx)
 
 class CRFModel(nn.Module):
     def __init__(self, num_tags):
@@ -36,11 +28,11 @@ num_tags = len(tag_to_idx)
 model = CRFModel(num_tags)
 
 criterion = model.crf
-optimizer = optim.SGD(model.parameters(), lr = 0.01)
+optimizer = optim.SGD(model.parameters(), lr = 0.001)
 
-num_epochs = 1000
+num_epochs = 1
 for epoch in range(num_epochs):
-    model.train()
+    model.train()   
     total_loss = 0
     
     for inputs, targets in zip(X_train, Y_train_idx):
@@ -49,13 +41,14 @@ for epoch in range(num_epochs):
         
         optimizer.zero_grad()
         outputs = model(bruh)
+        print(outputs.shape)
         loss = -criterion(outputs, targets)
         loss.backward()
         optimizer.step()
         
         total_loss += loss.item()
     
-    avg_loss = total_loss / len(X_train_idx)
+    avg_loss = total_loss / len(X_train)
     print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {avg_loss:.4f}")
 
 while True:
@@ -73,5 +66,4 @@ while True:
 
     print(predicted_tags)
     predicted_tags = [list(tag_to_idx.keys())[idx] for idx in predicted_tags[0]]
-    print("Input Sentence:", " ".join(input_sentence))
     print("Predicted Tags:", " ".join(predicted_tags))
